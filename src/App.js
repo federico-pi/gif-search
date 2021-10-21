5ac7a69e5d86ac6e4bd3df087b7f094f6fb1e392
Fred Balzi <balzi.fred@gmail.com>
2021-10-21 19:55:16 +0200

    Search form improvements

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
 import SearchForm from './Components/SearchForm';
 import GifList from './Components/GifList';
 
@@ -8,19 +9,27 @@ export default class App extends Component {
   constructor() {
     super();
     this.state = {
-      gifs: []
+      gifs: [],
+      loading: true
     }
   } 
 
   componentDidMount() {
-    fetch('1Iif22NnuSxZ9E0dMDd2lH6UEwB2P3fb')
-      .then(response => response.json())
-      .then(responseData => {
-        this.setState({ gifs: responseData.data });
+    // axios.get('1Iif22NnuSxZ9E0dMDd2lH6UEwB2P3fb')
+    this.performSearch();
+  }
+
+  performSearch = (query = 'meme') => {
+    axios.get(`http://api.giphy.com/v1/gifs/search?q=${query}&limit=30&api_key=dc6zaTOxFJmzC`)
+      .then(response => {
+        this.setState({
+          gifs: response.data.data,
+          loading: false
+        })
       })
       .catch(error => {
-        console.log('Error fetching and passing data', error)
-      });
+        console.log('Error fetching and parsing data', error);
+      })
   }
 
   render() { 
@@ -29,11 +38,15 @@ export default class App extends Component {
         <div className="main-header">
           <div className="inner">
             <h1 className="main-title">GifSearch</h1>
-            <SearchForm />      
+            <SearchForm onSearch={this.performSearch} />      
           </div>   
         </div>    
         <div className="main-content">
-          <GifList />
+          {
+            (this.state.loading)
+              ? <p>Loading...</p>
+              : <GifList data={this.state.gifs} />
+          }
         </div>
       </div>
     );
